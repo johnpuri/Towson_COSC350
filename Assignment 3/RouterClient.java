e980fa36e9611feeb7535c726229948c81f655e4
johnpuri <70924381+johnpuri@users.noreply.github.com>
2024-09-08 01:23:58 -0400

    Add files via upload

---
+++ Assignment 3/RouterClient.java
@@ -0,0 +1,73 @@
+
+
+import java.io.DataOutputStream;
+import java.net.Socket;
+import java.util.*;
+
+/** This is referred to as R2:
+ * Order of input (with examples)
+ * 
+ * 
+ * R2 (client: node 6) asks the user to enter the information in the DVR message to be sent to R1 (server: node 7) as follows:
+ * 
+ * Enter number of entries in DVR message: 4
+ * 
+ * Enter destinations in DVR message separated by a single space: 6 3 2 5
+ * 
+ * Enter distances to each destination in DVR message separated by a single space: 0 2 8 28
+ * 
+ * R2 (client: node 6) uses the preceding information to form the DVR message, which consists of the pairs
+ * of (destination, distance) values (6, 0), (3, 2), (2, 8), (5, 28).
+ * 
+ * R2 (client: node 6) makes a TCP socket connection to R1 (server: node 7) on port 6789 and sends a message M containing
+ * the DVR message to R1. The message M consists of the number of entries in the DVR message followed by the (destination, distance)
+ * pairs in the DVR message. 
+ */
+public class RouterClient {
+    public static void main(String[] args) throws Exception{
+        
+        int[][] entries;
+        
+        Scanner scanner = new Scanner(System.in);
+        
+        System.out.print("Enter number of entries in DVR message: ");
+        entries = new int[scanner.nextInt()][2];
+        
+        System.out.print("Enter destinations in DVR message separated by a single space: ");
+        for(int i = 0; i < entries.length; i++){
+            entries[i][0] = scanner.nextInt();
+        }
+        
+        System.out.print("Enter distances to each destination in DVR message separated by a single space: ");
+        for(int i = 0; i < entries.length; i++){
+            entries[i][1] = scanner.nextInt();
+        }
+        
+        System.out.print("Sending the following pairs to the server... \n");
+        String sendToServer = printPairs(entries);
+        
+        Socket clientSocket = new Socket("localhost", 6789);
+        DataOutputStream outToServer = new DataOutputStream(clientSocket.getOutputStream());
+        
+        // The number of entries is implicitly sent by taking the length of the message
+        outToServer.writeBytes(sendToServer + '\n');
+        clientSocket.close();
+    }
+    
+    private static String printPairs(int[][] pairs){
+    	String string = "";
+    	
+        for(int i = 0; i < pairs.length; i++){
+            int[] ab = new int[2];
+            for(int j = 0; j < pairs[i].length; j++){
+                ab[j] = pairs[i][j];
+            }
+            String s = "(" + ab[0] + "," + ab[1] + ")";
+            System.out.print(s);
+            string += s;
+            if(i + 1 < pairs.length)
+            	string += " ";
+        }
+        return string;
+    }
+}
---
+++ Assignment 3/RouterServer.java
@@ -0,0 +1,136 @@
+
+
+import java.util.*;
+import java.io.*;
+import java.net.*;
+
+/** This is referred to as R1:
+ * Order of input (with examples)
+ * 
+ * 
+ * R1 (server: node 7) first asks the user to enter R1’s neighbor distances and R1’s routing table as follows.
+ * 
+ * Enter number of neighbors: 2
+ * Enter neighbor IDs separated by a single space: 6 4
+ * Enter distances to each neighbor separated by a single space: 5 3
+ * Enter number of destinations: 6
+ * Enter destinations separated by a single space: 7 4 6 3 2 5
+ * Enter next hop for each destination separated by a single space: 0 4 6 4 6 6
+ * Enter distance to each destination separated by a single space: 0 3 5 14 13 19
+ * 
+ * prints out the R1 routing table
+ * 
+ * makes tcp connection to R2
+ * 
+ * DVR algorithm happens
+ * 
+ * R1 prints its updated routing table
+ */
+public class RouterServer {
+    public static void main(String[] args) throws Exception {
+        
+        int[][] rTable; // this includes next hop and distances
+        int[] neighbors;
+        int[] neighborDist;
+        int[] destinations;
+        Scanner scanner = new Scanner(System.in);
+        
+        System.out.print("Follow the input exactly as the assignment lists it.\n");
+        
+        System.out.print("Enter number of neighbors: ");
+        neighbors = new int[scanner.nextInt()];
+        
+        System.out.print("Enter neighbor IDs separated by a single space: ");
+        for(int i = 0; i < neighbors.length; i++){
+            neighbors[i] = scanner.nextInt();
+        }
+        
+        System.out.print("Enter distances to each neighbor separated by a single space: ");
+        for(int i = 0; i < neighbors.length; i++){
+            int useless = scanner.nextInt();
+        }
+        
+        System.out.print("Enter number of destinations: ");
+        destinations = new int[scanner.nextInt()];
+        rTable = new int[destinations.length][3];
+        
+        System.out.print("Enter destinations separated by a single space: ");
+        for(int i = 0; i < destinations.length; i++){
+            destinations[i] = scanner.nextInt();
+            rTable[i][0] = destinations[i];
+        }
+        
+        System.out.print("Enter next hop for each destination separated by a single space: ");
+        for(int i = 0; i < rTable.length; i++){
+            rTable[i][1] = scanner.nextInt();
+        }
+        
+        System.out.print("Enter distance to each destination separated by a single space: ");
+        for(int i = 0; i < rTable.length; i++){
+            rTable[i][2] = scanner.nextInt();
+        }
+        
+        printRoutingTable(rTable);
+        
+        // TCP connection is made and receives client's data
+        ServerSocket welcomeSocket = new ServerSocket(6789);
+        String fromClient;
+        
+		while (true) {
+			Socket connectionSocket = welcomeSocket.accept();
+			BufferedReader inFromClient = new BufferedReader(new InputStreamReader(connectionSocket.getInputStream()));
+			fromClient = inFromClient.readLine();
+			System.out.println("Received: " + fromClient);
+
+			String[] pairsInString = fromClient.split(" ");        
+	        int[][] arrayFromClient = parseStringPairs(pairsInString);
+	        
+	        // The number of entries is implicitly received by taking 'fromClient.length'
+	        updateRoutingTable(6, rTable, arrayFromClient);
+	        
+	        printRoutingTable(rTable);
+		}
+		
+    }
+    
+    private static int[][] parseStringPairs(String[] pairs)
+    {
+    	int[][] goodForm = new int[pairs.length][2];
+    	for(int i = 0; i < pairs.length; i++)
+    	{
+    		String temp = pairs[i];
+    		char num1 = temp.charAt(1);
+    		char num2 = temp.charAt(3);
+    		goodForm[i][0] = Character.getNumericValue(num1);
+    		goodForm[i][1] = Character.getNumericValue(num2);
+    	}
+    	return goodForm;
+    }
+    
+    private static void printRoutingTable(int[][] table){
+        System.out.printf("%15s%15s%15s\n\n", "Destination", "Next Hop", "Distance");
+        for(int i = 0; i < table.length; i++){
+            for(int j = 0; j < table[i].length; j++){
+                System.out.printf("%15d", table[i][j]);
+            }
+            System.out.print("\n\n");
+        }
+    }
+    
+    private static void updateRoutingTable(int node, int[][] table, int[][] fromNeighbor){
+        int distToNode = -999;
+        for(int i = 0; i < table.length; i++){
+            if(table[i][0] == 6) distToNode = table[i][2];
+        }
+        for(int i = 0; i < fromNeighbor.length; i++){
+            for(int j = 0; j < table.length; j++){
+                if(fromNeighbor[i][0] == node) break;
+                else if(fromNeighbor[i][0] == table[j][0]){
+                    if(table[j][2] >= distToNode + fromNeighbor[i][1])
+                        table[j][1] = node;
+                    table[j][2] = distToNode + fromNeighbor[i][1];
+                }
+            }
+        }
+    }
+}
